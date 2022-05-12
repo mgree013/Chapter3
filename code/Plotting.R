5fe2a260c0ebc5980c9b9ff6c915eaef740c61ba
mgree013 <mgree013@ucr.edu>
2022-05-12 14:36:49 -0400

    fig 4 edit

--- code/Plotting.R
+++ code/Plotting.R
@@ -35,6 +35,12 @@ points(dune.nmds$points[,1], dune.nmds$points[,2],  pch = 1)
 ordihull(dune.nmds, sp_abund_env$actual_fish_presence, display="sites", label=F,lwd=2, col=c("#440154FF","#FDE725FF"))
 ordisurf(dune.nmds, sp_abund_env$lake_elevation_nbr, prioirty=,labcex=0.9, add = T,col="forestgreen")
 
+line = 1
+cex = 1
+side = 3
+adj=-0.05
+mtext("a)", side=side, line=line, cex=cex, adj=adj)
+
 #stream
 set.seed(99)
 species<-species_2
@@ -48,7 +54,11 @@ plot(dune.nmds,typ= "n", xlab = "NMDS Axis 1", ylab = "NMDS Axis 2")
 points(dune.nmds$points[,1], dune.nmds$points[,2],  pch = 1) 
 ordihull(dune.nmds, species_env$Fish, display="sites", label=F,lwd=2, col=c("#440154FF","#FDE725FF"))
 ordisurf(dune.nmds, species_env$Elevation, prioirty=,labcex=0.9, add = T,col="forestgreen")
-
+line = 1
+cex = 1
+side = 3
+adj=-0.05
+mtext("b)", side=side, line=line, cex=cex, adj=adj)
 
 
 dev.off()
