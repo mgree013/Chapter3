50ff65a6dbf9450fd8c941ad7228c98ae7013880
mgree013 <mgree013@ucr.edu>
2022-05-19 13:17:05 -0400

    glm results supp

--- code/Plotting.R
+++ code/Plotting.R
@@ -27,17 +27,17 @@ sp_abund_env_filter<-sp_abund_env%>%filter(lake_elevation_nbr >1800, lake_elevat
 #lake
 set.seed(99)
 species<-sp_abund_env_filter[,3:39]
-dune.rel<-decostand(species,"total") #standardize community data
-dune.bray<-vegdist(dune.rel) #calculate dissimilarity among sites (i.e. dissimilarity matrix)
-dune.nmds=metaMDS(dune.rel, k=2, try=1000) #NMDS code
-dune.nmds
+lake.rel<-decostand(species,"total") #standardize community data
+lake.bray<-vegdist(lake.rel) #calculate dissimilarity among sites (i.e. dissimilarity matrix)
+lake.nmds=metaMDS(lake.bray, k=2, try=1000) #NMDS code
+lake.nmds
 #stressplot(dune.nmds)
 
-plot(dune.nmds,typ= "n", xlab = "NMDS Axis 1", ylab = "NMDS Axis 2")
+plot(lake.nmds,typ= "n", xlab = "NMDS Axis 1", ylab = "NMDS Axis 2")
 #text(dune.nmds$species[,1], dune.nmds$species[,2], rownames(dune.nmds$species), cex=0.7, col ="black")
-points(dune.nmds$points[,1], dune.nmds$points[,2],  pch = 1) 
-ordihull(dune.nmds, sp_abund_env_filter$actual_fish_presence, display="sites", label=F,lwd=2, col=c("#440154FF","#FDE725FF"))
-ordisurf(dune.nmds, sp_abund_env_filter$lake_elevation_nbr, prioirty=,labcex=0.9, add = T,col="forestgreen")
+points(lake.nmds$points[,1], lake.nmds$points[,2],  pch = 1) 
+ordihull(lake.nmds, sp_abund_env_filter$actual_fish_presence, display="sites", label=F,lwd=2, col=c("#440154FF","#FDE725FF"))
+ordisurf(lake.nmds, sp_abund_env_filter$lake_elevation_nbr, prioirty=,labcex=0.9, add = T,col="forestgreen")
 
 line = 1
 cex = 1
@@ -52,9 +52,9 @@ species_env<-species_env%>%filter(Elevation >3200)%>%dplyr::select(c(Elevation,S
 set.seed(99)
 species<-species_2%>%rownames_to_column("Site")%>%left_join(env_elevation, by="Site")%>%
   filter(Elevation >3200)%>%dplyr::select(-c(Elevation))%>%column_to_rownames("Site")#%>%dplyr::select(-c(Chironomidae,Nematomorpha,Oligochaeta,Ostracoda,Turbellaria,Euhirudinea))
-dune.rel<-decostand(species,"total") #standardize community data
+dune.rel<-decostand(species,"hellinger") #standardize community data
 dune.bray<-vegdist(dune.rel) #calculate dissimilarity among sites (i.e. dissimilarity matrix)
-dune.nmds=metaMDS(dune.rel, k=2, try=1000) #NMDS code
+dune.nmds=metaMDS(dune.bray, k=2, try=1000) #NMDS code
 dune.nmds
 #stressplot(dune.nmds) 
 
--- code/Stats.Models.R
+++ code/Stats.Models.R
@@ -10,7 +10,12 @@ mod2<-glm(N1~lake_elevation_nbr*actual_fish_presence,family =gaussian(link="iden
 mod3<-glm(N1~1,family =gaussian(link="identity"), data=env_div)
 reported.table2 <- bbmle::AICtab(mod0,mod1,mod2,mod3,weights = TRUE, sort = FALSE)
 reported.table2
+check_collinearity(mod2)
+multicollinearity(mod2)
+r2(mod0)
 r2(mod1)
+r2(mod2)
+r2(mod3)
 
 mod0<-betareg(betas.LCBD~actual_fish_presence, data=env_div)
 mod1<-betareg(betas.LCBD~lake_elevation_nbr, data=env_div)
@@ -18,7 +23,10 @@ mod2<-betareg(betas.LCBD~lake_elevation_nbr*actual_fish_presence, data=env_div)
 mod3<-betareg(betas.LCBD~1, data=env_div)
 reported.table2 <- bbmle::AICtab(mod0,mod1,mod2,mod3,weights = TRUE, sort = FALSE)
 reported.table2
+r2(mod0)
 r2(mod1)
+r2(mod2)
+r2(mod3)
 
 mod0<-glm(Com.Size~actual_fish_presence,family = gaussian(link="identity"), data=env_div)
 mod1<-glm(Com.Size~lake_elevation_nbr,family = gaussian(link="identity"), data=env_div)
@@ -26,7 +34,10 @@ mod2<-glm(Com.Size~lake_elevation_nbr*actual_fish_presence,family =gaussian(link
 mod3<-glm(Com.Size~1,family =gaussian(link="identity"), data=env_div)
 reported.table2 <- bbmle::AICtab(mod0,mod1,mod2,mod3,weights = TRUE, sort = FALSE)
 reported.table2
+r2(mod0)
 r2(mod1)
+r2(mod2)
+r2(mod3)
 
 mod0<-glm(Biomass~actual_fish_presence,family = gaussian(link="identity"), data=env_div)
 mod1<-glm(Biomass~lake_elevation_nbr,family = gaussian(link="identity"), data=env_div)
@@ -34,7 +45,10 @@ mod2<-glm(Biomass~lake_elevation_nbr*actual_fish_presence,family =gaussian(link=
 mod3<-glm(Biomass~1,family =gaussian(link="identity"), data=env_div)
 reported.table2 <- bbmle::AICtab(mod0,mod1,mod2,mod3,weights = TRUE, sort = FALSE)
 reported.table2
+r2(mod0)
 r2(mod1)
+r2(mod2)
+r2(mod3)
 
 ###########################################################################
 #Streams
@@ -44,7 +58,10 @@ mod2<-glm(N1~Elevation*Fish,family =gaussian(link="identity"), data=diversity.en
 mod3<-glm(N1~1,family =gaussian(link="identity"), data=diversity.env)
 reported.table2 <- bbmle::AICtab(mod0,mod1,mod2,mod3,weights = TRUE, sort = FALSE)
 reported.table2
+r2(mod0)
 r2(mod1)
+r2(mod2)
+r2(mod3)
 
 mod0<-betareg(betas.LCBD~Fish, data=diversity.env)
 mod1<-betareg(betas.LCBD~Elevation, data=diversity.env)
@@ -52,7 +69,10 @@ mod2<-betareg(betas.LCBD~Elevation*Fish, data=diversity.env)
 mod3<-betareg(betas.LCBD~1, data=diversity.env)
 reported.table2 <- bbmle::AICtab(mod0,mod1,mod2,mod3,weights = TRUE, sort = FALSE)
 reported.table2
+r2(mod0)
 r2(mod1)
+r2(mod2)
+r2(mod3)
 
 mod0<-glm(Com.Size~Fish,family = gaussian(link="identity"), data=diversity.env)
 mod1<-glm(Com.Size~Elevation,family = gaussian(link="identity"), data=diversity.env)
@@ -60,7 +80,10 @@ mod2<-glm(Com.Size~Elevation*Fish,family =gaussian(link="identity"), data=divers
 mod3<-glm(Com.Size~1,family =gaussian(link="identity"), data=diversity.env)
 reported.table2 <- bbmle::AICtab(mod0,mod1,mod2,mod3,weights = TRUE, sort = FALSE)
 reported.table2
+r2(mod0)
 r2(mod1)
+r2(mod2)
+r2(mod3)
 
 mod0<-glm(Biomass~Fish,family = gaussian(link="identity"), data=diversity.env)
 mod1<-glm(Biomass~Elevation,family = gaussian(link="identity"), data=diversity.env)
@@ -68,19 +91,79 @@ mod2<-glm(Biomass~Elevation*Fish,family =gaussian(link="identity"), data=diversi
 mod3<-glm(Biomass~1,family =gaussian(link="identity"), data=diversity.env)
 reported.table2 <- bbmle::AICtab(mod0,mod1,mod2,mod3,weights = TRUE, sort = FALSE)
 reported.table2
+r2(mod0)
 r2(mod1)
+r2(mod2)
+r2(mod3)
 
 ######################################################################################################################################################
 #Permanova/Dispersion: NMDS
 
+#lakes
+a<-adonis2(lake.bray ~ sp_abund_env_filter$actual_fish_presence+sp_abund_env_filter$lake_elevation_nbr, permutations = 99, method = "bray")
+summary(a)
+betad <- betadiver(dune.rel, "z")
+adonis(betad ~ sp_abund_env$actual_fish_presence+sp_abund_env$lake_elevation_nbr, data=species, perm=200)
+mod <- betadisper(lake.bray, sp_abund_env_filter$actual_fish_presence)
+anova(mod)
+print(mod)
+permutest(mod)
+boxplot(mod)
+
+#Stream
+adonis2(dune.bray ~ species_env$Fish+species_env$Elevation, permutations = 99, method = "bray")
+betad <- betadiver(dune.bray, "z")
+adonis(betad ~ species_env$Fish+species_env$Elevation, data=species, perm=200)
+mod <- betadisper(dune.bray, species_env$Fish)
+anova(mod)
+print(mod)
+permutest(mod)
+boxplot(mod)
+
+
+
 ######################################################################################################################################################
 #CWM
 
+#lakes
+mod0<-glm(CWM~actual_fish_presence,family = gaussian(link="identity"), data=env_cwm)
+mod1<-glm(CWM~lake_elevation_nbr,family = gaussian(link="identity"), data=env_cwm)
+mod2<-glm(CWM~lake_elevation_nbr*actual_fish_presence,family =gaussian(link="identity"), data=env_cwm)
+mod3<-glm(CWM~1,family =gaussian(link="identity"), data=env_cwm)
+reported.table2 <- bbmle::AICtab(mod0,mod1,mod2,mod3,weights = TRUE, sort = FALSE)
+reported.table2
+r2(mod0)
+r2(mod1)
+r2(mod2)
+r2(mod3)
+
+#Streams
+mod0<-glm(Body_mass_mg~Fish,family = gaussian(link="identity"), data=datasz)
+mod1<-glm(Body_mass_mg~Elevation,family = gaussian(link="identity"), data=datasz)
+mod2<-glm(Body_mass_mg~Elevation*Fish,family =gaussian(link="identity"), data=datasz)
+mod3<-glm(Body_mass_mg~1,family =gaussian(link="identity"), data=datasz)
+reported.table2 <- bbmle::AICtab(mod0,mod1,mod2,mod3,weights = TRUE, sort = FALSE)
+reported.table2
+r2(mod0)
+r2(mod1)
+r2(mod2)
+r2(mod3)
 
 ######################################################################################################################################################
 #Beta-diversity
 
 ######################################################################################################################################################
 #Species Responses
+
+######################################################################################################################################################
+#Supplemental
+
+#lakes
+mod0<-glm(lake_elevation_nbr~actual_fish_presence,family= gaussian(link = "identity") ,env_div)
+summary(mod0)
+
+#streams
+mod0<-glm(Elevation~Fish,family= gaussian(link = "identity") ,stream.elev)
+summary(mod0)
 
 ######################################################################################################################################################
